40fe31eb67725ecf8a140c13a5dbb2dfb743e30f
Sayani Koley <sayanikoley8@gmail.com>
2021-06-09 16:22:23 +0530

    Calculated distance of the line

--- LineComparisonComputation.java
+++ LineComparisonComputation.java
@@ -1,5 +1,21 @@
+import java.util.*;
+import java.lang.Math.*;
+
 public class LineComparisonComputation {
 	public static void main(String args[]) {
-		System.out.println("Welcome to Line Comparison Computation Program");
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the value of x1: ");
+                float x1=sc.nextFloat();
+	        System.out.print("Enter the value of y1: ");
+	        float y1=sc.nextFloat();
+                System.out.print("Enter the value of x2: ");
+	        float x2=sc.nextFloat();
+ 		System.out.print("Enter the value of y2: ");
+	        float y2=sc.nextFloat();
+	  	    
+		double distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+ 	 	
+		System.out.println("The 2 points of the line are: (" +x1+ "," +y1+ ") and (" +x2+ "," +y2+ ")");	 	    
+  	        System.out.println("Length of the line is: " +distance+ " units.");	
 	}
 }
